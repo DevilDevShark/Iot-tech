58336d03ba8369b7ba663935c6a015b836d49427
kevin roland <36869574+DevilDevShark@users.noreply.github.com>
2023-02-24 22:13:25 +0100

    [Update]: directed by Jesus

--- IOT-Laser/src/main.cpp
+++ IOT-Laser/src/main.cpp
@@ -1,12 +1,18 @@
 #include <HardwareSerial.h>
 
 int laser = 17;
+int bouton = 17;
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(bouton, INPUT);
   pinMode(laser, OUTPUT);
   Serial.println("Starting");
+}
+
+void  start(){
+
 }
 
 void loop()
@@ -14,31 +20,33 @@ void loop()
   int data = 0b11010;
   int data2 = 0b11001;
 
-  for (int i = 0; i < 10; i++) {
-      digitalWrite(laser, HIGH);
-      delay(100);
-      digitalWrite(laser, LOW);      
-      delay(100);
-    }
-
-  Serial.println("Debut");
+  //Serial.println("Debut");
 
   //la trame doit durée 200ms
   for (int i = 4; i >= 0; i--) {
     int bit = (data2 >> i) & 1;
-    Serial.println(bit);
+    //Serial.println(bit);
 
     if (bit == 0) {
       digitalWrite(laser, LOW);
-      delay(40);
+      delay(20);
     } else {
       digitalWrite(laser, HIGH);
-      delay(40);
+      delay(20);
     }
+    int buttonState = digitalRead(bouton);
+
+   if (buttonState == 0) {
+    digitalWrite(laser, HIGH);
+    Serial.println(bit);
+  } else {
+   digitalWrite(laser, LOW);
+   }
   }
-  Serial.println("Fin");
-  digitalWrite(laser, LOW);
-  delay(5000);
+  //Serial.println("Fin");
+    //digitalWrite(laser, LOW);
+  //  delay(5000);
+
 }
 
 void sendDataUsingLaser()
