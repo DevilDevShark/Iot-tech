2627689593cedd25ce25b997a5a55fbea27007bd
Yann Attakoui <yannatkk@gmail.com>
2023-03-02 12:01:42 +0100

    refactor: code review

--- IOT-Laser/src/main.cpp
+++ IOT-Laser/src/main.cpp
@@ -1,18 +1,18 @@
 #include <HardwareSerial.h>
 
-int laser = 17;
-int bouton = 17;
+int laserPin = 17;
+int buttonPin = 17;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(bouton, INPUT);
-  pinMode(laser, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(laserPin, OUTPUT);
   Serial.println("Starting");
 }
 
-void  start(){
-
+void start()
+{
 }
 
 void loop()
@@ -20,35 +20,31 @@ void loop()
   int data = 0b11010;
   int data2 = 0b11001;
 
-  //Serial.println("Debut");
-
-  //la trame doit durée 200ms
-  for (int i = 4; i >= 0; i--) {
+  // la trame doit durée 200ms
+  for (int i = 4; i >= 0; i--)
+  {
     int bit = (data2 >> i) & 1;
-    //Serial.println(bit);
 
-    if (bit == 0) {
-      digitalWrite(laser, LOW);
-      delay(20);
-    } else {
-      digitalWrite(laser, HIGH);
+    if (bit == 0)
+    {
+      digitalWrite(laserPin, LOW);
       delay(20);
     }
-    int buttonState = digitalRead(bouton);
+    else
+    {
+      digitalWrite(laserPin, HIGH);
+      delay(20);
+    }
+    int buttonState = digitalRead(buttonPin);
 
-   if (buttonState == 0) {
-    digitalWrite(laser, HIGH);
-    Serial.println(bit);
-  } else {
-   digitalWrite(laser, LOW);
-   }
+    if (buttonState == 0)
+    {
+      digitalWrite(laserPin, HIGH);
+      Serial.println(bit);
+    }
+    else
+    {
+      digitalWrite(laserPin, LOW);
+    }
   }
-  //Serial.println("Fin");
-    //digitalWrite(laser, LOW);
-  //  delay(5000);
-
-}
-
-void sendDataUsingLaser()
-{
 }
